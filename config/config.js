84b8e985354f3dadb109ba8c960a231ee2caf766
huanghuilian <35296397+huanghuilian@users.noreply.github.com>
2018-04-16 23:06:43 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 const config = {
-    appID:"wx6d05981d96407f2c",
-    appsecret:"4744011b5c7f4e2ff57ca06b57d4eb1e",
+    appID:"xxxxxxxxxxxx",
+    appsecret:"xxxxxxxxxxx",//需要按实际填写
     token:"hhl951HHL"
 }
 module.exports = config;
